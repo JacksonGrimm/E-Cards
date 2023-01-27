2f48f616d7ffb14a307dca125d486c438d632d68
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2023-01-27 11:10:17 -0700

    added graphql files

--- server/server.js
+++ server/server.js
@@ -2,15 +2,17 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 
-const db = require("./config/connection");
+// const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+let typeDefs = "";
+let resolvers = "";
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
 });
 
 app.use(express.urlencoded({ extended: false }));
