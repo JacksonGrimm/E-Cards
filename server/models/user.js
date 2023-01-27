13c4716fd2f3a533cd0c820b027a4c219b824c3e
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2023-01-27 14:37:02 -0700

    added password validation

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,22 @@
 const { Schema, model } = require("mongoose");
 const { tradesSchema } = require("./trades");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
   name: {
     type: String,
     required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    match: /.+\@.+\..+/,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 5,
   },
   profilePicture: {
     type: String,
@@ -28,6 +40,16 @@ const userSchema = new Schema({
     get: (timeStamp) => new Date(timeStamp).toDateString(),
   },
 });
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = model("user", userSchema);
 module.exports = { User };
